f60fbc442ed4b39084ace2c81a4fc84d2fd7500c
Yi Zhao <yizhao.cs@gmail.com>
2014-10-31 13:26:37 -0700

    added ConvertToByteArray.imageToByteArray

--- src/ConvertToByteArray.java
+++ src/ConvertToByteArray.java
@@ -1,6 +1,36 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.apache.commons.io.IOUtils;
+
+
 
 public class ConvertToByteArray {
-	public static byte[] stringToByteArray(String s){
+	public static byte[] stringToByteArray(String s) {
 		return s.getBytes();
+	}
+
+	public static byte[] imageToByteArray() throws IOException {
+		byte[] res = null;
+		FileInputStream in = null;
+
+		try {
+			in = new FileInputStream("image.png");
+
+			res = IOUtils.toByteArray(in);
+
+		} catch (FileNotFoundException e) {
+			System.out.println("Caught FileNotFoundException: " + e.getMessage());
+		} catch (IOException e) {
+			System.out.println("Caught IOException: " + e.getMessage());
+		} finally {
+			if (in != null) {
+				in.close();
+			}
+		}
+		return res;
 	}
 }
