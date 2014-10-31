e29db76d34886776c8c61008c8d2d18a20a03c1c
Yi Zhao <yizhao.cs@gmail.com>
2014-10-31 13:27:33 -0700

    1

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		String time = GetCurServerTime.getCurServerTime(); // 1414785544
-		byte[] stringToByteArray = ConvertToByteArray.stringToByteArray("abc"); // [97, 98, 99]
+		byte[] stringToByteArray = ConvertToByteArrayOrBinaryData.stringToByteArray("abc"); // [97, 98, 99]
 		String byteArrayToString = ConvertToString.byteArrayToString(stringToByteArray); // abc
-		byte[] imageToByteArray = ConvertToByteArray.imageToByteArray(); // [-119, 80, 78, 71, 13, 10, 26, blablablabla
-		
+		byte[] imageToByteArray = ConvertToByteArrayOrBinaryData.imageToByteArray(); // [-119, 80, 78, 71, 13, 10, 26, blablablabla
+
 		System.out.println(time);
 		System.out.println(Arrays.toString(stringToByteArray));
 		System.out.println(byteArrayToString);
