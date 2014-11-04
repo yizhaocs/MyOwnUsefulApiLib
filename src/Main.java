88d9be3904507b6cd69f7173f5505e5b6bb34909
Yi Zhao <yizhao.cs@gmail.com>
2014-11-04 10:51:57 -0800

    Added ConvertByteArrayToHexString

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.util.Arrays;
 
 import ConversionOfByteArray.ConvertByteArrayToFile;
+import ConversionOfByteArray.ConvertByteArrayToHexString;
 import ConversionOfByteArray.ConvertByteArrayToString;
 import ConversionOfByteArray.ConvertFileToByteArray;
 
@@ -9,15 +10,17 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		String time = GetCurServerTime.getCurServerTime(); // 1414785544
 		byte[] stringToByteArray = ConvertFileToByteArray.stringToByteArray("abc"); // [97, 98, 99]
-		String byteArrayToString = ConvertByteArrayToString.byteArrayToString(stringToByteArray); // abc
-		byte[] imageToByteArray = ConvertFileToByteArray.imageToByteArray(); // [-119, 80, 78, 71, 13, 10, 26, blablablabla
-		ConvertByteArrayToFile.byteArrayToFile(imageToByteArray);
-		String uuidGenerator = UuidGenerator.generateUuid(); // 36962753-7a15-4996-b100-fd132166cac5
-
-		System.out.println(time);
-		System.out.println(Arrays.toString(stringToByteArray));
-		System.out.println(byteArrayToString);
-		System.out.println(Arrays.toString(imageToByteArray));
-		System.out.println(uuidGenerator);
+//		String byteArrayToString = ConvertByteArrayToString.byteArrayToString(stringToByteArray); // abc
+		System.out.println(ConvertByteArrayToHexString.byteArrayToHexString(stringToByteArray)); // 616263
+//		byte[] imageToByteArray = ConvertFileToByteArray.imageToByteArray(); // [-119, 80, 78, 71, 13, 10, 26, blablablabla
+		
+//		ConvertByteArrayToFile.byteArrayToFile(imageToByteArray);
+//		String uuidGenerator = UuidGenerator.generateUuid(); // 36962753-7a15-4996-b100-fd132166cac5
+//
+//		System.out.println(time);
+//		System.out.println(Arrays.toString(stringToByteArray));
+//		System.out.println(byteArrayToString);
+//		System.out.println(Arrays.toString(imageToByteArray));
+//		System.out.println(uuidGenerator);
 	}
 }
