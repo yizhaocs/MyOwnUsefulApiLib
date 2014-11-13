20eb308121c71c9b85fc164819f8a9c0dbf5e7be
Yi Zhao <yizhao.cs@gmail.com>
2014-11-13 11:54:35 -0800

    1

--- src/BuilderOfMysqlQuery/SingletonOfQueryBuilder.java
+++ src/BuilderOfMysqlQuery/SingletonOfQueryBuilder.java
@@ -1,6 +1,5 @@
 package BuilderOfMysqlQuery;
 
-
 public class SingletonOfQueryBuilder {
 	private static SingletonOfQueryBuilder instance = null;
 
@@ -30,6 +29,15 @@ public class SingletonOfQueryBuilder {
 		return sb.toString();
 	}
 
+	/*
+	 * String tableName = "tableName";
+	 * String[] insertColumnsWithoutUserKey = { "A", "B", "C", "D", "E" };
+	 * String[] valuesWithoutUserKey = { "'packageName'", "'streamKey'", "'hex'", "12345", "67890" };
+	 * String[] updateColumns = { "A", "B" };
+	 */
+	/*
+	 * INSERT INTO tableName(A,B,C,D,E) VALUES ('packageName','streamKey','hex',12345,67890) ON DUPLICATE KEY UPDATE A=VALUES(A),B=VALUES(B);
+	 */
 	protected String upsert(String table, String[] insertColumns, String[] values, String[] updateColumns) {
 		StringBuilder sb = new StringBuilder();
 		sb.append("INSERT INTO ");
@@ -66,8 +74,15 @@ public class SingletonOfQueryBuilder {
 	}
 
 	/*
+	 * String tableName = "tableName";
+	 * String[] columnsWillBeReturned = { "A", "B" };
+	 * String[] whereClauseCoulmns = { "C", "D", "E", "F" };
+	 * String[] whereClauseOperator = { "=", "=", "=", ">" };
+	 * String[] whereClauseValues = { "'ZZZ'", "'YYY'", "'XXX'", "5" };
+	 */
+	/*
 	 * SELECT A,B FROM tableName WHERE C='ZZZ' AND D='YYY' AND E='XXX' AND F>5;
-	 * */
+	 */
 	protected String select(String[] columnsWillBeReturned, String table, String[] whereClauseCoulmns, String[] whereClauseOperator, String[] whereClauseValues) {
 		StringBuilder sb = new StringBuilder();
 		sb.append("SELECT ");
@@ -85,7 +100,7 @@ public class SingletonOfQueryBuilder {
 		return sb.toString();
 	}
 
-	protected String update(String table, String[] updateCoulmns, String[] updateValues, String[] whereClauseCoulmns, String[]  whereClauseOperator,String[] whereClauseValues) {
+	protected String update(String table, String[] updateCoulmns, String[] updateValues, String[] whereClauseCoulmns, String[] whereClauseOperator, String[] whereClauseValues) {
 		StringBuilder sb = new StringBuilder();
 		sb.append("UPDATE ");
 		sb.append(table);
@@ -99,22 +114,31 @@ public class SingletonOfQueryBuilder {
 			}
 		}
 		sb.append(" WHERE ");
-		sb.append(whereClauseBuilder(whereClauseCoulmns,whereClauseOperator, whereClauseValues));
-		sb.append(";");
-		return sb.toString();
-	}
-	
-	protected String delete(String table, String[] whereClauseCoulmns, String[] whereClauseOperator,String[] whereClauseValues) {
-		StringBuilder sb = new StringBuilder();
-		sb.append("DELETE FROM ");
-		sb.append(table);
-		sb.append(" WHERE ");
-		sb.append(whereClauseBuilder(whereClauseCoulmns, whereClauseOperator,whereClauseValues));
+		sb.append(whereClauseBuilder(whereClauseCoulmns, whereClauseOperator, whereClauseValues));
 		sb.append(";");
 		return sb.toString();
 	}
 
-	protected String whereClauseBuilder(String[] whereClauseCoulmns,String[] whereClauseOperator, String[] whereClauseValues) {
+	/*
+	 * String tableName = "tableName";
+	 * String[] whereClauseCoulmns = { "C", "D", "E", "F" };
+	 * String[] whereClauseOperator = { "=", "=", "=", ">" };
+	 * String[] whereClauseValues = { "'ZZZ'", "'YYY'", "'XXX'", "5" };
+	 */
+	/*
+	 * DELETE FROM tableName WHERE C='ZZZ' AND D='YYY' AND E='XXX' AND F>5;
+	 */
+	protected String delete(String table, String[] whereClauseCoulmns, String[] whereClauseOperator, String[] whereClauseValues) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("DELETE FROM ");
+		sb.append(table);
+		sb.append(" WHERE ");
+		sb.append(whereClauseBuilder(whereClauseCoulmns, whereClauseOperator, whereClauseValues));
+		sb.append(";");
+		return sb.toString();
+	}
+
+	protected String whereClauseBuilder(String[] whereClauseCoulmns, String[] whereClauseOperator, String[] whereClauseValues) {
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < whereClauseCoulmns.length; i++) {
 			sb.append(whereClauseCoulmns[i]);
