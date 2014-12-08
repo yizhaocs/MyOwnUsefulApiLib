82f83ec77f6e7021c929990f6cb71e00a41a4491
Yi Zhao <yizhao.cs@gmail.com>
2014-12-07 17:13:23 -0800

    1

---
+++ src/Callback/Main.java
@@ -0,0 +1,19 @@
+package Callback;
+
+/**
+ * Created by yizhao on 12/7/14.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Exam exam = new Exam();
+        //注册监听器
+        exam.registerStudentName(new Student() {
+            @Override
+            public void myName() {
+                System.out.println("Yi Zhao");
+            }
+        });
+        //模拟用户点击
+        exam.getStudentName();
+    }
+}
