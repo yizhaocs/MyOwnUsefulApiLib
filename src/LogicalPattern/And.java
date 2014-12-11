fa69f8f2cf10ddaf79fa7dc8236f7a979fe55cca
Yi Zhao <yizhao.cs@gmail.com>
2014-12-10 19:19:52 -0800

    1

---
+++ src/LogicalPattern/And.java
@@ -0,0 +1,32 @@
+package LogicalPattern;
+
+/**
+ * Created by yizhao on 12/10/14.
+ */
+public class And {
+    public static void main(String[] args){
+        and_pattern_1();
+        and_pattern_2();
+    }
+
+    public static void and_pattern_1() {
+        boolean A = true;
+        boolean B = true;
+
+        if(A && B){
+            System.out.println("statement");
+        }
+    }
+
+    public static void and_pattern_2() {
+        boolean A = true;
+        boolean B = true;
+
+        if(A){
+            if(B){
+                System.out.println("statement");
+            }
+        }
+    }
+
+}
---
+++ src/LogicalPattern/Or.java
@@ -0,0 +1,33 @@
+package LogicalPattern;
+
+/**
+ * Created by yizhao on 12/10/14.
+ */
+public class Or {
+    public static void main(String[] args){
+        or_pattern_1();
+        or_pattern_2();
+    }
+
+    public static void or_pattern_1() {
+        boolean A = true;
+        boolean B = false;
+
+        if(A || B){
+            System.out.println("statement");
+        }
+    }
+
+    public static void or_pattern_2() {
+        boolean A = true;
+        boolean B = false;
+
+        if(A){
+            System.out.println("statement");
+        }else{
+            if(B){
+                System.out.println("statement");
+            }
+        }
+    }
+}
