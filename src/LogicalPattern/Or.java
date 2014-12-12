ef9b38764a37990d640814faf2e759a4a68d6b90
Yi Zhao <yizhao.cs@gmail.com>
2014-12-11 19:12:38 -0800

    1

--- src/LogicalPattern/Or.java
+++ src/LogicalPattern/Or.java
@@ -9,29 +9,57 @@ public class Or {
    * Pattern: A||B
    * */
     public static void main(String[] args){
-        or_pattern_1();
-        or_pattern_2();
+        or_pattern_A_1();
+        or_pattern_A_2();
+        or_pattern_B_1();
+        or_pattern_B_2();
     }
 
-    public static void or_pattern_1() {
+    public static void or_pattern_A_1() {
         boolean A = true;
         boolean B = false;
 
         if(A || B){
-            System.out.println("statement");
+            System.out.println("statement_A");
         }
     }
 
-    public static void or_pattern_2() {
+    public static void or_pattern_A_2() {
         boolean A = true;
         boolean B = false;
 
         if(A){
-            System.out.println("statement");
+            System.out.println("statement_A");
         }else{
             if(B){
-                System.out.println("statement");
+                System.out.println("statement_A");
             }
+        }
+    }
+
+    public static void or_pattern_B_1() {
+        boolean A = true;
+        boolean B = false;
+
+        if(A || B){
+            System.out.println("statement_A");
+            return;
+        }
+        System.out.println("statement_B");
+    }
+
+    public static void or_pattern_B_2() {
+        boolean A = true;
+        boolean B = false;
+
+        if(A){
+            System.out.println("statement_A");
+        }else{
+            if(B){
+                System.out.println("statement_A");
+                return;
+            }
+            System.out.println("statement_B");
         }
     }
 }
