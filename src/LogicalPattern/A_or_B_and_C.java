818b44fd7c233dfca4cfd935480ad7f13cc4a9b0
Yi Zhao <yizhao.cs@gmail.com>
2014-12-12 10:01:14 -0800

    1

--- src/LogicalPattern/A_or_B_and_C.java
+++ src/LogicalPattern/A_or_B_and_C.java
@@ -7,8 +7,9 @@ public class A_or_B_and_C {
     /*
 * Pattern: (A||B)&&C
 * */
-    public static void main(String[] args){
+    public static void main(String[] args) {
         pattern_1();
+        pattern_2();
 
     }
 
@@ -16,17 +17,31 @@ public class A_or_B_and_C {
         boolean A = true;
         boolean B = true;
         boolean C = true;
-        if(A){
-            if(C){
+        if (A) {
+            if (C) {
                 System.out.println("statement_1");
             }
-        }else{
-            if(B){
-                if(C){
+        } else {
+            if (B) {
+                if (C) {
                     System.out.println("statement_1");
                 }
             }
         }
     }
 
+    public static void pattern_2() {
+        boolean A = true;
+        boolean B = true;
+        boolean C = true;
+        if (A) {
+            if (C) {
+                System.out.println("statement_1");
+            }
+        } else if (B) {
+            if (C) {
+                System.out.println("statement_1");
+            }
+        }
+    }
 }
