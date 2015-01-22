ea286326a8cd33e7a2c3d98e007b70466493638b
Yi Zhao <yizhao.cs@gmail.com>
2015-01-21 17:11:02 -0800

    1

---
+++ src/J2EE_API_Designs/Constants.java
@@ -0,0 +1,22 @@
+package J2EE_API_Designs;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Created by yizhao on 1/21/15.
+ */
+public class Constants {
+    private static final Logger log = Logger.getLogger(Constants.class);
+
+    // generic error code
+    public static final String STATUS_CODE_SUCCESS = "0";
+
+    public static final String STATUS_CODE_INTERNAL_SERVER_ERROR = "103";
+
+    public static final String STATUS_CODE_APIKEY_INVALID = "104";
+
+    public static final String STATUS_CODE_APIKEY_NULL = "108";
+
+    public static final String INVALID_SESSION_OR_BAD_SESSION_AUTH_PAIR = "120";
+
+}
---
+++ src/J2EE_API_Designs/StatusResult.java
@@ -0,0 +1,28 @@
+package J2EE_API_Designs;
+
+/**
+ * Created by yizhao on 1/21/15.
+ */
+public class StatusResult {
+    private String status;
+
+    public StatusResult() {
+        this.status = Constants.STATUS_CODE_SUCCESS;
+    }
+
+    public StatusResult(String status) {
+        this.status = status;
+    }
+
+    public StatusResult(int status) {
+        this.status = Integer.toString(status);
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
