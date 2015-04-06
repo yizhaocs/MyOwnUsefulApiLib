4be197117800b951d31e5eb035f7c565367f477c
Yi Zhao <yizhao.cs@gmail.com>
2015-04-06 16:51:17 -0700

    1

--- HadoopApis/src/main/java/WordCount.java
+++ HadoopApis/src/main/java/WordCount.java
@@ -3,6 +3,7 @@ package main.java;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -16,48 +17,51 @@ import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class WordCount {
-    public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
+
+    public static class mMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
         private final static IntWritable one = new IntWritable(1);
         private Text word = new Text();
 
-        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            StringTokenizer itr = new StringTokenizer(value.toString());
-            while (itr.hasMoreTokens()) {
-                word.set(itr.nextToken());
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            String line = value.toString();
+            StringTokenizer tokenizer = new StringTokenizer(line);
+            while (tokenizer.hasMoreTokens()) {
+                word.set(tokenizer.nextToken());
                 context.write(word, one);
             }
         }
     }
 
-    public static class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
-        private IntWritable result = new IntWritable();
-
+    public static class mReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
         public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
             int sum = 0;
             for (IntWritable val : values) {
                 sum += val.get();
             }
-            result.set(sum);
-            context.write(key, result);
+            context.write(key, new IntWritable(sum));
         }
     }
 
     public static void main(String[] args) throws Exception {
+        //String hadoopFS = "hdfs://localhost:9000/";
+        //String sequenceFileName = "/fuhu/logs/kafka/logging_consumer/temp/test.log";
         Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "word count");
-        job.setJarByClass(WordCount.class);
-        job.setMapperClass(TokenizerMapper.class);
-        job.setCombinerClass(IntSumReducer.class);
-        job.setReducerClass(IntSumReducer.class);
+        //conf.set("fs.defaultFS", hadoopFS);
+        //org.apache.hadoop.fs.Path path = new org.apache.hadoop.fs.Path(sequenceFileName);
+
+        Job job = new Job(conf, "wordcount");
+
+        job.setMapperClass(mMapper.class);
+        job.setReducerClass(mReducer.class);
         //此处的设置是最终输出的key/value，一定要注意！
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
         //再次理解此处设置的输入输出格式。。。它表示的是一种对文件划分，索引的方法
-		job.setInputFormatClass(TextInputFormat.class);
-		job.setOutputFormatClass(TextOutputFormat.class);
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
         FileInputFormat.addInputPath(job, new Path("/Users/yizhao/Desktop/hello.txt"));
         FileOutputFormat.setOutputPath(job, new Path("/Users/yizhao/Desktop/output"));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
-
 }
