50db00f0f525697d864eaf76a38112eef69587f4
Yi Zhao <yizhao.cs@gmail.com>
2015-04-09 16:06:08 -0700

    1

--- Spring3QuartzScheduler/src/main/java/RunMeJob.java
+++ Spring3QuartzScheduler/src/main/java/RunMeJob.java
@@ -5,17 +5,24 @@ import org.quartz.JobExecutionException;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
 /* Reference: http://www.mkyong.com/spring/spring-quartz-scheduler-example/*/
+
+/**
+ * The QuartzJobBean is more flexible and suitable for complex scheduler.
+ * You need to create a class extends the Spring’s QuartzJobBean,
+ * and define the method you want to schedule in executeInternal() method,
+ * and pass the scheduler task (RunMeTask) via setter method.
+ */
 public class RunMeJob extends QuartzJobBean {
-	private RunMeTask runMeTask;
+    private RunMeTask runMeTask;
 
-	public void setRunMeTask(RunMeTask runMeTask) {
-		this.runMeTask = runMeTask;
-	}
+    public void setRunMeTask(RunMeTask runMeTask) {
+        this.runMeTask = runMeTask;
+    }
 
-	protected void executeInternal(JobExecutionContext context)
-			throws JobExecutionException {
+    protected void executeInternal(JobExecutionContext context)
+            throws JobExecutionException {
 
-		runMeTask.printMe();
+        runMeTask.printMe();
 
-	}
+    }
 }
--- Spring3QuartzScheduler/src/main/java/RunMeTask.java
+++ Spring3QuartzScheduler/src/main/java/RunMeTask.java
@@ -1,8 +1,12 @@
 package main.java;
 
 /* Reference: http://www.mkyong.com/spring/spring-quartz-scheduler-example/*/
+
+/**
+ * Create a normal Java class, this is the class you want to schedule in Quartz.
+ */
 public class RunMeTask {
-	public void printMe() {
-		System.out.println("Spring 3 + Quartz 1.8.6 ~");
-	}
+    public void printMe() {
+        System.out.println("Spring 3 + Quartz 1.8.6 ~");
+    }
 }
