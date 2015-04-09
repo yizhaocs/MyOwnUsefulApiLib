658a41f50143d7bbb0e7d7ab26a5239048f9c274
Yi Zhao <yizhao.cs@gmail.com>
2015-04-09 11:19:47 -0700

    1

---
+++ HadoopApis/src/main/java/TextFileToSequenceFile.java
@@ -0,0 +1,42 @@
+package main.java;
+
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+
+import java.io.IOException;
+
+/**
+ * Created by yizhao on 4/7/15.
+ */
+public class TextFileToSequenceFile {
+    public static class BinaryFilesToHadoopSequenceFileMapper extends Mapper<Object, Text, Text, Text> {
+        public void map(Object key, Text value, Context context)
+                throws IOException, InterruptedException {
+            context.write(new Text("a"), value);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = new Job(conf, "BinaryFilesToHadoopSequenceFile");
+        job.setJarByClass(BinaryFilesToHadoopSequenceFileMapper.class);
+        job.setMapperClass(BinaryFilesToHadoopSequenceFileMapper.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(SequenceFileOutputFormat.class);
+        String srcPath = "/Users/yizhao/Desktop/a.txt";
+        String dstPath = "/Users/yizhao/Desktop/output";
+        FileInputFormat.setInputPaths(job, new Path(srcPath));
+        FileOutputFormat.setOutputPath(job, new Path(dstPath));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
