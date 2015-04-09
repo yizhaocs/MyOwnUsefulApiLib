81ee25be6a1d72254ecf500d07446faa6f3fae5b
Yi Zhao <yizhao.cs@gmail.com>
2015-04-09 15:26:05 -0700

    1

--- HadoopApis/src/main/java/MergeSequenceFilesUsingMapreduce.java
+++ HadoopApis/src/main/java/MergeSequenceFilesUsingMapreduce.java
@@ -3,7 +3,7 @@ package main.java;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.BytesWritable;
-import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
@@ -17,19 +17,20 @@ import java.io.IOException;
  * Created by yizhao on 4/6/15.
  */
 public class MergeSequenceFilesUsingMapreduce {
-    public static class mMapper extends Mapper<Object, BytesWritable, Text, BytesWritable> {
+    public static class mMapper extends Mapper<Object, BytesWritable, NullWritable, BytesWritable> {
         public void map(Object key, BytesWritable value, Context context)
                 throws IOException, InterruptedException {
-            context.write(new Text("MergeSequenceFilesUsingMapreduce"), value);
+            context.write(NullWritable.get(), value);
         }
     }
 
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
-        Job job = new Job(conf, "TextFileToSequenceFile");
+        Job job = new Job(conf, "MergeSequenceFilesUsingMapreduce");
         job.setJarByClass(mMapper.class);
         job.setMapperClass(mMapper.class);
-        job.setOutputKeyClass(Text.class);
+        //job.setNumReduceTasks(1);
+        job.setOutputKeyClass(NullWritable.class);
         job.setOutputValueClass(BytesWritable.class);
         job.setInputFormatClass(SequenceFileInputFormat.class);
         job.setOutputFormatClass(SequenceFileOutputFormat.class);
