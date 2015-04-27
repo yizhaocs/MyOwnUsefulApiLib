df5e17f0136b7328124157ad3d96ba7f7f82f6b7
Yi Zhao <yizhao.cs@gmail.com>
2015-04-27 11:13:57 -0700

    added Gson module

---
+++ Gson/src/main/java/BagOfPrimitives.java
@@ -0,0 +1,15 @@
+package main.java;
+
+/**
+ * Created by yizhao on 4/27/15.
+ */
+public class BagOfPrimitives {
+    private int value1 = 1;
+    private String value2 = "abc";
+    // private transient int value3 = 3;
+    private int[] value3 = { 1, 2, 3, 4 };
+
+    BagOfPrimitives() {
+        // no-args constructor
+    }
+}
---
+++ Gson/src/main/java/Main.java
@@ -0,0 +1,113 @@
+package main.java;
+
+import java.awt.Window.Type;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+/*
+ * Reference: https://sites.google.com/site/gson/gson-user-guide
+ * */
+public class Main {
+    static Gson gson = new Gson();
+
+    public static void main(String[] args) {
+        SerializationPrimitives();
+        DeserializationPrimitives();
+        SerializationObject();
+        DeserializationObject();
+        SerializationArray();
+        DeserializationArray();
+        SerializationCollections();
+        DeserializationCollections();
+
+    }
+
+    private static void SerializationPrimitives() {
+        System.out.println(gson.toJson(1)); // 1
+        System.out.println(gson.toJson("abcd")); // "abcd"
+        System.out.println(gson.toJson(new Long(10))); // 10
+        System.out.println(gson.toJson(true)); // true
+    }
+
+    private static void DeserializationPrimitives() {
+        int oneInt = gson.fromJson("1", int.class);
+        Integer oneInteger = gson.fromJson("1", Integer.class);
+        Long oneLong = gson.fromJson("1", Long.class);
+        Boolean falseBoopean = gson.fromJson("false", Boolean.class);
+        String str = gson.fromJson("\"abc\"", String.class);
+        // String anotherStr = gson.fromJson("[\"abc\"]", String.class);
+
+        System.out.println(oneInt); // 1
+        System.out.println(oneInteger); // 2
+        System.out.println(oneLong); // 2
+        System.out.println(falseBoopean); // false
+        System.out.println(str); // abc
+        // System.out.println(anotherStr);
+    }
+
+    private static void SerializationObject() {
+        BagOfPrimitives obj = new BagOfPrimitives();
+        String json = gson.toJson(obj);
+        System.out.println(json); // {"value1":1,"value2":"abc","value3":[1,2,3,4]}
+    }
+
+    private static void DeserializationObject() {
+        BagOfPrimitives obj = new BagOfPrimitives();
+        String json = gson.toJson(obj);
+        System.out.println(json); // {"value1":1,"value2":"abc","value3":[1,2,3,4]}
+        BagOfPrimitives obj2 = gson.fromJson(json, BagOfPrimitives.class);
+        String json2 = gson.toJson(obj2);
+        System.out.println(json2); // {"value1":1,"value2":"abc","value3":[1,2,3,4]}
+    }
+
+    private static void SerializationArray() {
+        int[] ints = { 1, 2, 3, 4, 5 };
+        String[] strings = { "abc", "def", "ghi" };
+        int[][] doubleArray = { { 1 }, { 2, 3 } };
+        System.out.println(gson.toJson(ints)); // [1,2,3,4,5]
+        System.out.println(gson.toJson(strings)); // ["abc","def","ghi"]
+        System.out.println(gson.toJson(doubleArray)); // [[1],[2,3]]
+    }
+
+    private static void DeserializationArray() {
+        int[] ints = gson.fromJson("[1,2,3,4,5]", int[].class);
+        String[] strings = gson.fromJson("[\"abc\",\"def\",\"ghi\"]", String[].class);
+        int[][] doubleArray = gson.fromJson("[[1],[2,3]]", int[][].class);
+        System.out.println(Arrays.toString(ints)); // [1,2,3,4,5]
+        System.out.println(Arrays.toString(strings)); // ["abc","def","ghi"]
+        for (int[] singleArray : doubleArray) {
+            // [1]
+            // [2, 3]
+            System.out.println(Arrays.toString(singleArray));
+        }
+    }
+
+    private static void SerializationCollections() {
+        List<Integer> ints = new LinkedList<>();
+        ints.add(1);
+        ints.add(2);
+        ints.add(3);
+        ints.add(4);
+        ints.add(5);
+        String json = gson.toJson(ints);
+        System.out.println(json); // [1,2,3,4,5]
+    }
+
+    private static void DeserializationCollections() {
+        List<Integer> ints = new LinkedList<>();
+        ints.add(1);
+        ints.add(2);
+        ints.add(3);
+        ints.add(4);
+        ints.add(5);
+        String json = gson.toJson(ints);
+        java.lang.reflect.Type collectionType = new TypeToken<Collection<Integer>>(){}.getType();
+        List<Integer> ints2 = gson.fromJson(json, collectionType);
+        System.out.println(ints2); // [1, 2, 3, 4, 5]
+    }
+}
